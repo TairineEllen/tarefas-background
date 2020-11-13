3d876d7aa2c28e734f53a63c2b160ee8038f2368
TairineEllen <tairine.ellen@gmail.com>
2020-11-13 15:14:22 -0300

    Criada pasta de controllers com método POST para inserir usuário com nome, email e senha randômica

--- src/server.js
+++ src/server.js
@@ -1,9 +1,13 @@
 import 'dotenv/config';
 import express from 'express';
 
+import controllerUsers from './app/controllers/controllerUsers';
+
 const app = express();
 
 app.use(express.json());
+
+app.post('/users', controllerUsers.store);
 
 app.listen(process.env.PORT, () => {
   console.log(`Servidor rodando na porta ${process.env.PORT}`);
